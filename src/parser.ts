7b59a23328f2cb789323cf876b223cfc1779f3bf
koolii <kuriyama@createentrance.com>
2018-02-02 13:46:13 +0100

    :+1: implement Parser class constructor

---
+++ src/parser.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs-extra'
+import { createInterface, ReadLine } from 'readline'
+
+export default class Parser {
+  readLine: ReadLine
+  filepath: string
+
+  constructor(filepath: string) {
+    this.filepath = filepath
+  }
+
+  load() {
+    const stream: fs.ReadStream = fs.createReadStream(`${__dirname}/${this.filepath}`, 'utf-8')
+    const readLine: ReadLine = createInterface({
+      input: stream,
+    })
+
+    readLine.on('line', (line) => {
+      console.log(`coming a line: ${line}`)
+    })
+
+    this.readLine = readLine;
+  }
+}
