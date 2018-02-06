f4a0e36db1db1fc74f4e4d5002be9bd472f0ec4a
koolii <kuriyama@createentrance.com>
2018-02-06 08:00:52 +0100

    :+1: [WIP] implement Parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,24 @@
 import * as fs from 'fs-extra'
 import { createInterface, ReadLine } from 'readline'
 
-// A_COMMAND: @Xxxを表し、Xxxはシンボルか10進数の数
-const A_COMMAND: RegExp = /^@(\w+)/
-// C_COMMAND: dest=comp:jump(destもしくはjumpのどちらかは空であるかもしれない、destが空なら`=`が、jumpが空なら`;`が省略される)
-const C_COMMAND: RegExp = /[=|;]/
-// L_COMMAND: 疑似コマンド`(Xxx)`を意味し、Xxxはシンボルとなる
-const L_COMMAND: RegExp = /\((\w+)\)/
+
+const COMMAND = {
+  // A_COMMAND: @Xxxを表し、Xxxはシンボルか10進数の数
+  A: {
+    reg: /^@(\w +) /,
+    type: 'A_COMMAND',
+  },
+  // C_COMMAND: dest=comp:jump(destもしくはjumpのどちらかは空であるかもしれない、destが空なら`=`が、jumpが空なら`;`が省略される)
+  C: {
+    reg: /[=|;]/,
+    type: 'C_COMMAND',
+  },
+  // L_COMMAND: 疑似コマンド`(Xxx)`を意味し、Xxxはシンボルとなる
+  L: {
+    reg: /\((\w+)\)/,
+    type: 'L_COMMAND',
+  },
+}
 
 export default class Parser {
   readLine: ReadLine
@@ -29,6 +41,36 @@ export default class Parser {
     this.readLine = readLine;
   }
 
-  hasMoreCommands() {
+  parse(line: string) {
+    if (this.hasMoreCommands(line)) {
+      this.advance(line)
+    }
+  }
+
+  hasMoreCommands(line: string) {
+    return line.match(COMMAND.A.reg) || line.match(COMMAND.C.reg) || line.match(COMMAND.L.reg)
+  }
+
+  advance(line: string) {
+    const type = this.commandType(line)
+    const symbol = [COMMAND.A.type, COMMAND.L.type].includes(type) ? this.symbol(line, type) : null
+  }
+
+  commandType(line: string) {
+    const a = line.match(COMMAND.A.reg)
+    if (a) {
+      return a[1]
+    }
+    const c = line.match(COMMAND.C.reg)
+    if (c) {
+      return c[1]
+    }
+    const l = line.match(COMMAND.L.reg)
+    if (a) {
+      return l[1]
+    }
+  }
+
+  symbol(line: string, type: string) {
   }
 }
