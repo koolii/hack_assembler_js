a00ac273cdd402baf938f6ff1380a04cd54bf896
koolii <koolii0909@gmail.com>
2018-02-11 03:45:36 +0100

    :+1: add Code module(class) and interface which is used for returning Code.compile()

--- src/assembler.ts
+++ src/assembler.ts
@@ -1,27 +1,38 @@
 import Parser from './parser'
+import Code from './code'
+import { IParser } from './interface'
 import Logger from './logger'
+import contents from './constants'
 
 export default class HackAssembler {
   parser: Parser
-  log: Logger
+  code: Code
+  logger: Logger
 
   constructor() {
-    this.log = new Logger(HackAssembler)
+    this.logger = new Logger(HackAssembler)
     this.parser = new Parser(process.argv[2])
+    this.code = new Code()
   }
 
   async setup() {
-    this.log.setup('Start setup()')
+    this.logger.setup('Start setup()')
     await this.parser.readFile()
   }
 
   async exec() {
-    this.log.setup('Start exec()')
+    this.logger.setup('Start exec()')
 
     while (1) {
-      const parsed = this.parser.advance()
+      const parsed: IParser = this.parser.advance()
       if (!parsed) break
-      this.log.exec(JSON.stringify(parsed))
+      this.logger.exec(JSON.stringify(parsed))
+
+      const encoded = this.code.compile(parsed)
+      if (encoded) {
+        this.logger.exec(JSON.stringify(encoded))
+      }
+
     }
   }
 }
