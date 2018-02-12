5b2eda1fa3b386d23f41da96a2cdd32bfc4dd7b7
koolii <kuriyama@createentrance.com>
2018-02-12 13:38:33 +0100

    :+1: [WIP] add writer makes .hack files

---
+++ src/writer.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs-extra'
+import Logger from './logger'
+
+export default class Writer {
+  filepath: string
+  logger: Logger
+
+  constructor(filepath: string) {
+    this.filepath = filepath.replace('.asm', '.hack')
+    this.logger = new Logger(Writer)
+  }
+
+  async write(line: string) {
+    this.logger.write(`write line: ${line}`)
+    await fs.appendFile(this.filepath, `${line}\n`, { encoding: 'utf-8' })
+  }
+
+  async remove() {
+    this.logger.remove(this.filepath)
+    await fs.remove(this.filepath)
+  }
+}
