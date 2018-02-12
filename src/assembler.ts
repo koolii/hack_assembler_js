3d5176b5c5647bc9c229be481ca3cf5e08bfa349
koolii <kuriyama@createentrance.com>
2018-02-12 13:37:39 +0100

    :+1: encode 10 => binary code

--- src/assembler.ts
+++ src/assembler.ts
@@ -1,5 +1,6 @@
 import Parser from './parser'
 import Code from './code'
+import Writer from './writer'
 import { IParser } from './interface'
 import Logger from './logger'
 import contents from './constants'
@@ -7,11 +8,13 @@ import contents from './constants'
 export default class HackAssembler {
   parser: Parser
   code: Code
+  writer: Writer
   logger: Logger
 
   constructor() {
     this.logger = new Logger(HackAssembler)
     this.parser = new Parser(process.argv[2])
+    this.writer = new Writer(process.argv[2])
     this.code = new Code()
   }
 
@@ -23,6 +26,8 @@ export default class HackAssembler {
   async exec() {
     this.logger.setup('Start exec()')
 
+    await this.writer.remove()
+
     while (1) {
       const parsed: IParser = this.parser.advance()
       if (!parsed) break
@@ -31,8 +36,13 @@ export default class HackAssembler {
       const encoded = this.code.compile(parsed)
       if (encoded) {
         this.logger.exec(JSON.stringify(encoded))
+      } else if (parsed.command === contents.COMMAND.A.type) {
+        // symbolはまずは無視して、只単純にバイナリに変更を行なう
+        this.logger.exec(JSON.stringify(parsed))
+        this.logger.exec(Number(parsed.symbol).toString(2))
       }
 
+      await this.writer.write(JSON.stringify(parsed))
     }
   }
 }
