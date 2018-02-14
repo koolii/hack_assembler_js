31cc02af4ec022f7f053af48d71c01e2b5dffd03
koolii <kuriyama@createentrance.com>
2018-02-14 07:49:26 +0100

    :+1: implement SymbolTable and define constant SYMBOL

--- src/symbol-table.ts
+++ src/symbol-table.ts
@@ -2,12 +2,32 @@ import Logger from './logger'
 import constants from './constants'
 
 export default class SymbolTable {
+  logger: Logger
+  table: Object
+
   constructor() {
+    this.logger = new Logger(SymbolTable)
+    this.table = {}
+    Object.keys(constants.SYMBOL).forEach((key) => {
+      this.addEntry(key, constants.SYMBOL[key])
+    })
   }
-  addEntry() {
+
+  addEntry(symbol: string, decimalAddr: number) {
+    if (!this.containes(symbol)) {
+      this.table[symbol] = decimalAddr
+    }
   }
-  containes() {
+
+  containes(symbol: string) {
+    return !!this.table[symbol]
   }
-  getAddress() {
+
+  getAddress(symbol: string) {
+    return this.table[symbol]
+  }
+
+  printCurrent() {
+    this.logger.printCurrent(JSON.stringify(this.table))
   }
 }
