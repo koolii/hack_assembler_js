64a29a56fa73d9770439bdbd081cd29ab5e16e57
koolii <koolii0909@gmail.com>
2018-02-15 20:35:57 +0100

    :+1: implement Function Symbol registration

--- src/symbol-table.ts
+++ src/symbol-table.ts
@@ -16,9 +16,11 @@ export default class SymbolTable {
   }
 
   addEntry(symbol: string, decimalAddr: number|string) {
-    if (!this.containes(symbol)) {
-      this.table[symbol] = Utils.getPaddedBinary(decimalAddr)
+    if (this.containes(symbol)) {
+      throw new Error('it has already registered symbol table.')
     }
+    // this.table[symbol] = Utils.getPaddedBinary(decimalAddr)
+    this.table[symbol] = decimalAddr
   }
 
   containes(symbol: string) {
@@ -33,6 +35,7 @@ export default class SymbolTable {
     this.logger.printCurrent(JSON.stringify(this.table))
   }
 
+    // P74-P80参照
     // 定義済シンボル
     // ラベルシンボル
     //    (Xxx)と言う疑似コマンドはXxxと言うラベルになる
@@ -41,20 +44,40 @@ export default class SymbolTable {
     // todo 上記の様に区別が必要になってくるので、シンボルがどういうシンボルなのかを判定できなければならない
     // todo ここの処理を全てSymbolTableクラスに移す。がその兼ね合いで、Parser.addvance()の参照がないので
     // Parser.getReader()の時点で既にパースが完了しているものを配列として持っておいたほうが効率が良い
-  createTable(readLine: Function) {
-    let nextAddr = 1
+  createTableOnlyFunction(readLine: Function) {
+    let funcAddr = 0
     while (true) {
       const parsed: IParser = readLine()
+      // this.logger.createTable(JSON.stringify(parsed))
       if (!parsed) {
         break
       }
-      if (parsed.symbol) {
-        // 疑似コマンドの次のコマンドの位置を参照する
-        this.addEntry(parsed.symbol, nextAddr);
+      // 一応確認する
+      if (!parsed.symbol) {
+        continue
       }
-      nextAddr += 1
-    }
 
-    this.printCurrent()
+      // register symbol into SymbolTable
+      if (parsed.symbol && constants.COMMAND.L.type === parsed.command && !this.containes(parsed.symbol)) {
+        // 疑似コマンドの次のコマンドの位置を参照する
+        // これは次の行を表しているのか、0スタートからの次ということで1になるかわかっていないので調査する
+        funcAddr += 1
+        this.addEntry(parsed.symbol, funcAddr);
+      }
+
+      // // 変数か関数呼び出しで、登録するのは変数の方のみ、数字の場合は飛ばす
+      // if (constants.COMMAND.A.type === parsed.command && !this.containes(parsed.symbol)) {
+      //   const symbol = parsed.symbol
+      //   // symbolが数値だった場合
+      //   // @100
+      //   // M=AでMに100を代入
+      //   if (!isNaN(Number(symbol))) {
+      //     continue
+      //   }
+      //   // 変数定義
+      //   this.addEntry(symbol, variableAddr);
+      //   variableAddr += 1
+      // }
+    }
   }
 }
